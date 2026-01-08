6fd8d098aeef0ae6607f6ca9fb62a597848f0b62
jarpat2791 <jarpat2791@gmail.com>
2026-01-08 20:41:20 +0100

    Update service worker cache version and logs

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,31 @@
-const CACHE_NAME = 'senior-easy-v2.5';
+// service-worker.js
+const CACHE_NAME = 'senior-easy-v3.0';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'
+  './manifest.json'
 ];
 
-// Instalacja Service Worker
 self.addEventListener('install', event => {
-  console.log('Service Worker: Instalacja...');
+  console.log('[Service Worker] Instalacja');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Service Worker: Cachowanie plików');
+        console.log('[Service Worker] Cache\'owanie plików');
         return cache.addAll(urlsToCache);
       })
       .then(() => self.skipWaiting())
   );
 });
 
-// Aktywacja Service Worker
 self.addEventListener('activate', event => {
-  console.log('Service Worker: Aktywacja...');
+  console.log('[Service Worker] Aktywacja');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Usuwanie starego cache:', cache);
+            console.log('[Service Worker] Usuwanie starego cache:', cache);
             return caches.delete(cache);
           }
         })
@@ -39,28 +35,22 @@ self.addEventListener('activate', event => {
   return self.clients.claim();
 });
 
-// Przechwytywanie requestów
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Zwróć z cache jeśli istnieje
         if (response) {
           return response;
         }
         
-        // W przeciwnym razie pobierz z sieci
         return fetch(event.request)
           .then(response => {
-            // Sprawdź czy odpowiedź jest poprawna
             if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
             
-            // Sklonuj odpowiedź
             const responseToCache = response.clone();
             
-            // Dodaj do cache
             caches.open(CACHE_NAME)
               .then(cache => {
                 cache.put(event.request, responseToCache);
@@ -69,14 +59,12 @@ self.addEventListener('fetch', event => {
             return response;
           })
           .catch(() => {
-            // Jeśli offline i brak w cache, zwróć podstawową stronę
             return caches.match('./index.html');
           });
       })
   );
 });
 
-// Obsługa komunikatów
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
